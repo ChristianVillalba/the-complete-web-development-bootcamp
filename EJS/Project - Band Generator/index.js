d934199b9c74ead4df833e8aed0de3bf215d894a
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-08-31 06:37:33 +0100

    project almost done. I still need to generate band name

--- EJS/Project - Band Generator/index.js
+++ EJS/Project - Band Generator/index.js
@@ -8,15 +8,27 @@ const port = 3000;
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
+app.use(express.static("public"));
 
 //Step 4 - Add a dynamic year to the footer.
+const date = new Date();
+let year = date.getFullYear();
+
 //Hint: Google to find out how to get the current year using JS.
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  res.render("index.ejs"); 
 });
+
+app.post("/submit", (req, res) => {
+  // const numLetters = req.body["fName"].length + req.body["lName"].length;
+  res.render("index.ejs", { 
+    // numberOfLetters: numLetters, 
+  });
+});
+
 
 app.post("/submit", (req, res) => {
   //Step 2 - Make the generate name functionality work
