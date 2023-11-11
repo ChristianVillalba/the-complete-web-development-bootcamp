230f5d86b7f3d03549abad17b83480df64fc9c3c
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-11-11 07:21:35 +0000

    debbugged. API doesn't work

--- 08 - APIs/04 - REST APIs/solution.js
+++ 08 - APIs/04 - REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "27b05d2d-76d0-4a66-a543-bbae53d95019";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
