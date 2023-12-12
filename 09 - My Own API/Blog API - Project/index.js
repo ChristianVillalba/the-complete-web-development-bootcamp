e2311a56af4cc01028a23bc1243aecb733b79471
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-12-12 10:00:33 +0000

    adds /all & /:id. Not tested yet

--- 09 - My Own API/Blog API - Project/index.js
+++ 09 - My Own API/Blog API - Project/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 4000;
@@ -37,12 +38,42 @@ let lastId = 3;
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
-
+app.get("/all", async (req, res) => {
+  try {
+    const response = await axios.get("URL/all");
+    const result = response.data;
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
 //CHALLENGE 2: GET a specific post by id
+app.get("/:id", async (req, res) => {
+  try {
+
+    const id = parseInt(req.params.id);
+    // const postById = posts.find( (post) => post.id === id);
+
+    // const response = await axios.get("URL/:id");
+    const postById = await axios.get(posts.find( (post) => post.id === id) );
+    const result = postById.data;
+    
+    res.render("index.ejs", { data: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+});
 
 //CHALLENGE 3: POST a new post
 
