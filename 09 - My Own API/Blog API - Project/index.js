263b149f61c1d9d09e4aa1ee50ab7edf49751562
Christian Villalba UK <christianvillalba.uk@gmail.com>
2023-12-15 17:20:57 +0000

    adds http requests

--- 09 - My Own API/Blog API - Project/index.js
+++ 09 - My Own API/Blog API - Project/index.js
@@ -76,10 +76,49 @@ app.get("/:id", async (req, res) => {
 });
 
 //CHALLENGE 3: POST a new post
+app.post("/posts", (req,res) => { 
+  const newPost = {
+    id: posts.length + 1,  
+    title: req.body.title ,
+    content: req.body.content ,
+    author: req.body.author ,
+    date: req.body.date ,
+  };
+  posts.push(newPost);
+  res.json(newPost);
+  console.log(newPost)
+})
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req,res) => { 
+  const id = parseInt(req.params.id);
+  const postToBeUpdated = posts.find((post) => post.id === id);
+  const replacementPost = {
+    id: id,
+    title: req.body.title || postToBeUpdated.title,
+    content: req.body.content || postToBeUpdated.content,
+    author: req.body.author || postToBeUpdated.author,
+    postText: req.body.date || postToBeUpdated.date,
+
+  };
+  const searchIndex = posts.findIndex((post) => post.id === id);
+  posts[searchIndex] = replacementPost ;
+  res.json(replacementPost)
+})
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req,res) => { 
+  const id = parseInt(req.params.id);
+  const searchIndex = posts.findIndex((post) => post.id === id);
+  if (searchIndex > -1) {
+    posts.splice(searchIndex, 1); 
+    res.sendStatus(200);
+  } else {
+    res.status(404).json({error: `Post with id: ${id} not found. No posts were deleted.`})
+  }
+})
+
+
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
