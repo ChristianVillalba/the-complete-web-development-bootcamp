338d2bb7e5706d69bf100eadb6ea688138bb44f1
Christian Villalba UK <christianvillalba.uk@gmail.com>
2024-07-22 23:41:06 +0100

    continues

--- 10 - PostgreSQL/TravelTracker/index.js
+++ 10 - PostgreSQL/TravelTracker/index.js
@@ -21,9 +21,9 @@ db.connect()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// give access to the Homepage (/)
-app.get("/", async (req, res) => {
-  // query for the places we visited. Its a query
+
+app.get("/", async (req, res) => {     // access Homepage (/)
+  // query for the places we visited. It's a query
   const result = await db.query("SELECT country_code FROM visited_countries");
   // the result of the query is an array with JS Objects. we will use a forEach loop to access them
   let countries = [];
@@ -37,12 +37,13 @@ app.get("/", async (req, res) => {
   db.end();
 });
 
-app.post("/add", async (req, res) => {
-  const input = req.body["country"];
+app.post("/add", async (req, res) => {  // new route > index.ejs > form - /add
+  const input = req.body["country"];   // index.ejs > input: name = "country"
 
-  const result = await db.query(
+  // We use the above input to make a query to our DB. and store it inside results
+  const result = await db.query(    
     "SELECT country_code FROM countries WHERE country_name = $1",
-    [input]
+    [input]   // changes the placeholder $1 for the user's input
   );
 
   if (result.rows.length !== 0) {
@@ -61,27 +62,3 @@ app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
 
-
-// //INSERT new country
-// app.post("/add", async (req, res) => {
-//   const input = req.body["country"];
-
-//   const result = await db.query(
-//     "SELECT country_code FROM countries WHERE country_name = $1",
-//     [input]
-//   );
-
-//   if (result.rows.length !== 0) {
-//     const data = result.rows[0];
-//     const countryCode = data.country_code;
-
-//     await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [
-//       countryCode,
-//     ]);
-//     res.redirect("/");
-//   }
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server running on http://localhost:${port}`);
-// });
