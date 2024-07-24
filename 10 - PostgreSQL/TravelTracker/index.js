7c815cef90afb0bb56830cdecfc45c7486467746
Christian Villalba UK <christianvillalba.uk@gmail.com>
2024-07-24 23:26:53 +0100

    ready to start imporovements

--- 10 - PostgreSQL/TravelTracker/index.js
+++ 10 - PostgreSQL/TravelTracker/index.js
@@ -24,11 +24,10 @@ app.use(express.static("public"));
 
 app.get("/", async (req, res) => {     // access Homepage (/)
   // query for the places we visited. It's a query
-  const result = await db.query("SELECT country_code FROM visited_countries");
-  // the result of the query is an array with JS Objects. we will use a forEach loop to access them
+  const result = await db.query("SELECT country_code FROM visited_countries");     // the result of the query is an array with JS Objects. we will use a forEach loop to access them
+  
   let countries = [];
-  // access every value returned for the query and push it to the empty array: countries
-  result.rows.forEach((country) => {
+  result.rows.forEach((country) => {     // access every value returned for the query and push it to the empty array: countries
     countries.push(country.country_code);
   });
   console.log(result.rows); // not neccessary but useful to check how the strucure of the data looks like
@@ -37,15 +36,12 @@ app.get("/", async (req, res) => {     // access Homepage (/)
 });
 
 app.post("/add", async (req, res) => {  // new route > index.ejs > form - /add
-  const input = req.body["country"];   // index.ejs > input: name = "country"
-
-  // We use the above input to make a query to our DB. and store it inside result
+  const input = req.body["country"];   // index.ejs > input: name = "country".  We use the input to make a query to our DB. and store it inside result 
   const result = await db.query(    
     "SELECT country_code FROM countries WHERE country_name = $1",
     [input]   // changes the placeholder $1 for the user's input
   );
-  // result.rows contains what are we looking for
-  // if the result.rows is actually empty, it means we got a no match from this query.
+  // result.rows contains what are we looking for  if the result.rows is actually empty, it means we got a no match from this query.
   if (result.rows.length !== 0) {      // if we have a match !==0 , then we continue and grab the data
     const data = result.rows[0];      // we place the data at the first position of the row ...
     const countryCode = data.country_code;     // we get the country_code that is inside that piece of data
@@ -56,7 +52,6 @@ app.post("/add", async (req, res) => {  // new route > index.ejs > form - /add
     res.redirect("/");  // redirects to HOMEPAGE
   }
 });
-
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
